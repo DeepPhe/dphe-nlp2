8fa1c6214a20fb32e1b6cbb054a515f7cb748ebe
Sean Finan <sean.finan@childrens.harvard.edu>
2024-04-17 11:40:17 -0400

    utility to zip vumc files utility to fix some formatting in vumc docs

--- src/main/java/org/healthnlp/deepphe/nlp/util/VumcPatientSorter.java
+++ src/main/java/org/healthnlp/deepphe/nlp/util/VumcPatientSorter.java
@@ -2,10 +2,7 @@ package org.healthnlp.deepphe.nlp.util;
 
 import org.apache.ctakes.core.util.StringUtil;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
@@ -19,64 +16,11 @@ import java.util.zip.ZipOutputStream;
  */
 final public class VumcPatientSorter {
 
-   //  R200011990-19960826-AP-Surgical_Pathology-1-234587023459782349.txt
-   //  R200011990-19960826-HP-OPERATIVE_REPORT-1-707234572345897203.txt
-
-   //  PatientID-YYYYMMDD-Source-Doc_Type-1-EncounterNum??.txt
+   //  PatientID-YYYYMMDD-Category-Doc_Type-Index-EncounterNum??.txt
    //  !!!!  Sometimes Date also has time?:  "20160121_101600"
-   //  !!!!  Sometimes files have the same encounterNum, but a different preceding number (index?)
-   //  R200011990-19960826-HP-OPERATIVE_REPORT-1-707234572345897203.txt
-   //  R200011990-19960826-HP-OPERATIVE_REPORT-2-707234572345897203.txt
-   //  R200011990-19960826-HP-OPERATIVE_REPORT-3-707234572345897203.txt
-   //  !!!!  There are some problematic Doc_Types:
-   //  !!!!  Physician_Provider_SIGNED_orders_-__strong_Cancer_Chemotherapy__strong__[All_Orders_Unsigned]
-   //  !!!!  ...Pre-Admission...
-   //  !!!!  PID-Date-Source-MyHealthTeam@Vanderbilt_Plan_of_Care-1-EncounterNum.txt   always garbage!  Just a PID?
-   //  !!  "-Braden_Assessment-" useless.
-   //  !!!!  Some F'ups like ending with "1--345782378562385.txt"    (multiple dash)
-   //  !!!!  Some Spaces:  PID-Date-Social History-Doc_Type-1-EncounterNum??.txt
-   //  !!!!  Just use positions 0 (PID), 1 (Date), 2 (Source), n-2 (index?), n-1 (encounterNum?)
-   //  !!!!    If n-2 (index?) is empty, use n-3.  This can happen because of their F'up.
-   //  !!!!    Replace spaces, '&', ',', '(', '[', etc. with underscores.  Their naming sucks.
 
-   //  !!!!  Note texts have garbage like "1. javascript:top.vois_link_to_plan('ID_NUM','sp27');"
-   //  !! De-ID:
-   //  **ID-NUM             [ID***]
-   //  **NAME[AAA,BBB]      **NAME[ZZZ]    **NAME[AAA's M]   **NAME[NNN, OOO M]  **NAME[ZZZ, YYY XXX]  **NAME[M&M]
-   //  **DATE[Dec 16 62]    **DATE[Jul 01 2016]     **DATE[Sep 30 15]:30:00 CST 2016   **DATE[Sep 30 15]:30:00 CST 2016 03:30 PM
-   //  **AGE[in 50s]
-   //  **PHONE              **PHONEarno8fy         ***PHONE       **EMAIL
-   //  **INSTITUTION        **INSTITUTION1211
-   //  **PLACE              **ZIP-CODE             **ROOM      **STREET-ADDRESS
-
-   //  !! Sometimes the above are in parentheses or braces:  (**NAME[zzz, yyy])  {**NAME[AAA]}
-   //  !! There are valid multi-* terms:  ***Change:     ***Note:
-   //  !! Replace:
-   //  **ID-NUM , [ID***] : " ID123456789 "
-   //  **NAME[AAA,BBB] : " Josephine Claudwell "
-   //  **DATE[1 2 3] : " 1 2 3 "
-   //  **AGE[in 50s] : " in 50s "
-   //  **PHONE , ***PHONE , **EMAIL , **INSTITUTION , **PLACE , **Zip-CODE , **ROOM : " "
-
-   //  !! There are useless docs, such as those with "(fuel_reimbursement_form)" in the filename.
-   //  !! Check things like "-Nurse's_note-" "-port_draw-" "-PORT_DRAW-"
-   //  "-Core_Measures-" "-Research_Lab_Draw-" "-Clinical_communication_(Renewal_Approved)-"\
-   //  "-Provider_Communications-" "-parish_nurse_note-" "-Surgical_Teaching-"
-   //  "-Dentistry_Return_Clinic_Visit-" "-Dentistry_Procedure_Note-" "-20LABS-"
-   //  for valid information.
-   //  "-Psychiatry_Progress_Note-" does sometimes have cancer in patient history.
-
-   //  !!!!  A bunch of notes do not use period characters or even have a space at the end of many sentences!
-   //  !!!!  "She has not vomited todayDiarrhea todayPt is still requesting MRI, chemo today. Also wants " ...
-   //  !! Some notes have "<br>" codes.  Replace with newline.
-   //  !! Some notes have long strings of "----------------" and "+---------------+" without spaces.  Replace with NL.
-   //  !! Some notes have "|" and "||" (sometimes with dashes above) without spaces.  Replace with newline.
-   //  !! Lots of notes have no space before or after parentheses '(' ')'.  Add space.
-
-
+   static private int _fileCount = 0;
    static private int _badSplitCount = 0;
-   //  PatientID-YYYYMMDD-Source-Doc_Type-Index-EncounterNum??.txt
-   //  !!!!  Sometimes Date also has time?:  "20160121_101600"
 
    static private String[] splitFilename( final String oldFilename ) {
       final String[] splits = StringUtil.fastSplit( oldFilename, '-' );
@@ -87,11 +31,12 @@ final public class VumcPatientSorter {
       }
 
       // Only return filled splits 0, 1, 4, 5
-      final String[] wantedSplits = new String[ 4 ];
+      final String[] wantedSplits = new String[ 5 ];
       wantedSplits[ 0 ] = splits[ 0 ];
       wantedSplits[ 1 ] = splits[ 1 ];
-      wantedSplits[ 2 ] = splits[ splitCount - 2 ];
-      wantedSplits[ 3 ] = splits[ splitCount - 1 ];
+      wantedSplits[ 2 ] = splits[ 2 ];
+      wantedSplits[ 3 ] = splits[ splitCount - 2 ];
+      wantedSplits[ 4 ] = splits[ splitCount - 1 ];
       return wantedSplits;
    }
 
@@ -102,32 +47,83 @@ final public class VumcPatientSorter {
    static private String getDate( final String[] splits ) {
       final String dateTime = splits[ 1 ];
       final int timeSplit = dateTime.indexOf( '_' );
-      if ( timeSplit < 8 ) {
+      if ( timeSplit < 6 ) {
+         // Normally MMDDyyyy, but use 6 just in case it is MMDDyy
          return dateTime;
       }
-      return dateTime.substring( 0, 8 );
+      return dateTime.substring( 0, timeSplit );
    }
+
+   static private String getCategory( final String[] splits ) {
+      final String source = splits[ 2 ];
+      if ( source.isEmpty() ) {
+         return "Unknown";
+      }
+      return source.replace( ' ', '_' );
+   }
+
+   static private final char[] BAD_CHARS = { ' ', '&', '{', '}', '[', ']', '(', ')', '@' };
 
    static private String getNewFilename( final String[] splits ) {
       // Sometimes no index is given.
-      final String index = splits[ 2 ].isEmpty() ? "0" : splits[ 2 ];
+      final String index = splits[ 3 ].isEmpty() ? "0" : splits[ 2 ];
       // Remove the .txt filename extension;
-      final String encounter = splits[ 3 ].substring( 0, splits[ 3 ].length() - 4 );
-      return encounter + "_" + index + ".txt";
+      final String encounter = splits[ 4 ].substring( 0, splits[ 3 ].length() - 4 );
+      final String date = getDate( splits );
+      final String category = getCategory( splits );
+      String join = encounter + "-" + index + "-" + category + "-" + date + ".txt";
+      for ( char c : BAD_CHARS ) {
+         join = join.replace( c, '_' );
+      }
+      return join;
    }
 
+   static private String getZipEntryname( final String filename ) {
+      final String[] splits = splitFilename( filename );
+      final String patientId = getPatientId( splits );
+      final String newFilename = getNewFilename( splits );
+      return patientId + "/" + newFilename;
+   }
 
    static private void zipFiles( final File inputDir, final List<String> filenames,
                                  final String outputZipPath ) {
       try ( FileOutputStream outputStream = new FileOutputStream( outputZipPath );
             ZipOutputStream zipStream = new ZipOutputStream( outputStream ) ) {
          for ( String filename : filenames ) {
-            final String[] splits = splitFilename( filename );
-            final String patientId = getPatientId( splits );
-            final String newFilename = getNewFilename( splits );
-            final String zipEntryname = patientId + "/" + newFilename;
+            final String lower = filename.toLowerCase();
+            if ( lower.contains( "myhealthteam@vanderbilt_plan_of_care" )
+                  || lower.contains( "fuel_reimbursement_form" )
+                  || lower.contains( "-social_history-" )
+                  || lower.contains( "-intake_assessment-" )
+                  || lower.contains( "-braden_assessment-" )
+                  || lower.contains( "-port_draw-" )
+                  || lower.contains( "-research_lab_draw-" )
+                  || lower.contains( "-core_measures-" )
+                  || lower.contains( "-nurse" )
+                  || lower.contains( "-parish_nurse_note" )
+                  || lower.contains( "-phone_call-" )
+                  || lower.contains( "-prescription" )
+                  || lower.contains( "-immunizations-" )
+                  || lower.contains( "-anesthetic_care_record-" )
+                  // It is rare, but sometimes a clinical communication does contain info on mass or procedure.
+//                  || lower.contains( "-clinical_communication" )
+                  || lower.contains( "-clinical_communication_(" )
+                  || lower.contains( "-provider_communication" )
+                  || lower.contains( "-clinic_summary-" )
+                  || lower.contains( "-surgical_teaching" )
+                  || lower.contains( "-dentistry_" ) ) {
+               // 99% of the time the above are useless, discarding to prevent false positives.
+               // True Positives should be picked up in other notes.  Though some info is valid, no cancer mentions.
+               continue;
+            }
             final File fileToZip = new File( inputDir, filename );
-            final FileInputStream inputStream = new FileInputStream( fileToZip );
+//            final FileInputStream inputStream = new FileInputStream( fileToZip );
+            final String text = VumcDocFixer.fixDoc( fileToZip );
+            if ( text.isEmpty() ) {
+               continue;
+            }
+            final InputStream inputStream = new ByteArrayInputStream( text.getBytes() );
+            final String zipEntryname = getZipEntryname( filename );
             final ZipEntry zipEntry = new ZipEntry( zipEntryname );
             zipStream.putNextEntry( zipEntry );
             byte[] bytes = new byte[ 1024 ];
@@ -136,6 +132,7 @@ final public class VumcPatientSorter {
                zipStream.write( bytes, 0, length );
             }
             inputStream.close();
+            _fileCount++;
          }
       } catch ( IOException ioE ) {
          System.err.println( ioE.getMessage() );
@@ -152,7 +149,7 @@ final public class VumcPatientSorter {
       final String zipFileName = args[ 2 ];
       final File inputDir = new File( inputPath );
       if ( !inputDir.isDirectory() ) {
-         System.out.println( inputPath + " does not exist." );
+         System.out.println( "Input Dir " + inputPath + " does not exist." );
          System.exit( 0 );
       }
       final File outputDir = new File( outputPath );
@@ -165,6 +162,8 @@ final public class VumcPatientSorter {
       }
       final String outputZipPath = new File( outputDir, zipFileName ).getAbsolutePath();
       zipFiles( inputDir, filenames, outputZipPath );
+      System.out.println( "Files zipped: " + _fileCount );
+      System.out.println( "Bad Filename Splits: " + _badSplitCount );
    }
 
 }
